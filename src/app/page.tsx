664bec018524bbc5ade91089c787cdb7559a3dd7
Yash KC <yashchauhan.coder@gmail.com>
2024-12-02 17:30:24 +0530

    feat:add_card,accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client"
 import Image from "next/image";
 import EmailSVG from "@/assets/email.svg"
 import PhoneSVG from "@/assets/phone.svg"
 import LocationSVG from "@/assets/location.svg"
+import DownArrowSVG from "@/assets/down-arrow.svg"
+import ChevronDownSVG from "@/assets/chevron-down.svg"
+import AddPlusRoundSVG from "@/assets/add-square-round.svg"
+import { useState } from "react";
+
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="w-full h-screen bg-slate-200 flex justify-center items-center px-8 py-12 gap-12">
+    <div className="w-full h-screen bg-slate-200 flex justify-center items-center px-8 py-12 gap-12 font-sans">
       <div className="w-1/2 h-full bg-white rounded-md font-serif overflow-auto">
         <div className="w-full flex flex-col justify-start items-start ">
           <div className="flex flex-col justify-center items-center py-6 w-full bg-slate-800">
@@ -108,8 +116,83 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-1/2 h-full bg-white rounded-md">
+      <div className="w-1/2 h-full bg-white rounded-md px-4">
+        <div className="flex flex-col justify-start items-start gap-6 w-full py-4">
+          <button className={`w-full flex flex-col rounded-lg text-slate-900 overflow-hidden flex-col shadow-lg
+            border border-4 border-violet-100 px-6 py-3
+          `}
+          >
+            <p className="py-2 font-semibold text-2xl text-purple-800">Name</p>
+            <div className="flex flex-col gap-2 w-full">
+              <div className="flex gap-1 justify-start items-center">
+                <Image src={EmailSVG} alt="Email" className="w-4 h-4" />
+                <p>yash@gmail.com</p>
+              </div>
+              <div className="flex gap-1 justify-start items-center">
+                <Image src={PhoneSVG} alt="Email" className="w-4 h-4" />
+                <p>+91 9900992233</p>
+              </div>
+              <div className="flex gap-1 justify-start items-center">
+                <Image src={LocationSVG} alt="Email" className="w-4 h-4" />
+                <p>Mumbai, India</p>
+              </div>
+            </div>
+          </button>
+          <div className={`w-full flex justify-between items-center  rounded-lg text-slate-900 overflow-hidden flex-col shadow-lg
+            border border-4 border-violet-100
+          `}
+          >
+            <button className={`flex justify-between items-center w-full px-4 py-2
+            ${open ? 'bg-violet-100' : ''}
+            `}
+              onClick={() => setOpen(!open)}
+            >
+              {open ?
+                <input value={'Experience'} placeholder="Section Name" className="text-xl w-fit px-3 py-1 bg-violet-50 rounded focus:outline-none"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }}
+                /> :
+                <span className="text-xl">Experience</span>
+              }
+              <Image src={ChevronDownSVG} alt="Down Arrow" className={`w-6 h-6 ${open ? 'rotate-180' : 'rotate-0'}`} />
+            </button>
+            <div className={`w-full flex justify-start items-start flex-col  transition-all duration-300 ease-in-out
+            ${open ? ' overflow-auto h-auto' : 'h-0 overflow-hidden'}
+            `}>
+              <button className="px-4 py-3 border-violet-100  border-b-4 w-full">
+                <div className="flex items-start flex-col">
+                  <p className="font-semibold text-slate-600">ABC</p>
+                  <p className="text-slate-600">Software Developer</p>
+                </div>
+              </button>
+              <div className="py-3 flex justify-center items-center w-full">
+                <button className="flex justify-center items-center gap-2 bg-violet-50 px-4 py-1 rounded-lg opacity-70 hover:opacity-100">
+                  <Image src={AddPlusRoundSVG} alt="Add" className="w-5" />
+                  <span className="text-lg">Experience</span>
+                </button>
 
+              </div>
+              {/* {
+                open ?
+                  <div className="w-full flex justify-start items-start py-2 px-3 flex-col ">
+                    <p>Hello world</p>
+                    <p>Hello world</p>
+                    <p>Hello world</p>
+                    <p>Hello world</p>
+                    <p>Hello world</p>
+
+                  </div>
+                  : <></>
+              } */}
+
+
+            </div>
+
+
+          </div>
+        </div>
       </div>
 
     </div>

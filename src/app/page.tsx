0277bcfd6f5a17aa31cad39c58ef63deebc44ecf
Yash KC <yashchauhan.coder@gmail.com>
2024-12-02 15:51:53 +0530

    feat:resume_template_ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,117 @@
 import Image from "next/image";
+import EmailSVG from "@/assets/email.svg"
+import PhoneSVG from "@/assets/phone.svg"
+import LocationSVG from "@/assets/location.svg"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full h-screen bg-slate-200 flex justify-center items-center px-8 py-12 gap-12">
+      <div className="w-1/2 h-full bg-white rounded-md font-serif overflow-auto">
+        <div className="w-full flex flex-col justify-start items-start ">
+          <div className="flex flex-col justify-center items-center py-6 w-full bg-slate-800">
+            <p className="font-bold text-2xl">Yash Chauhan</p>
+            <p className="text-slate-200">Full Stack Developer</p>
+            <div className="flex flex-wrap justify-center gap-3 text-slate-300 mt-2">
+              <div className="flex gap-1 justify-center items-center">
+                <Image src={EmailSVG} alt="Email" className="w-4 h-4" />
+                <p>yash@gmail.com</p>
+              </div>
+              <div className="flex gap-1 justify-center items-center">
+                <Image src={PhoneSVG} alt="Email" className="w-4 h-4" />
+                <p>+91 9900992233</p>
+              </div>
+              <div className="flex gap-1 justify-center items-center">
+                <Image src={LocationSVG} alt="Email" className="w-4 h-4" />
+                <p>Mumbai, India</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-start gap-3 items-start w-full px-8 py-6 text-slate-900">
+            <section className="w-full flex-col justify-start items-start">
+              <p className="font-bold text-md uppercase">Education</p>
+              <div className="w-12 h-1 bg-slate-900 rounded"></div>
+              <div className="flex flex-col gap-2">
+                <section className="w-full flex-col justify-start items-start text-sm">
+                  <div className="flex justify-between items-start py-1">
+                    <div className="flex flex-col">
+                      <p className="font-semibold ">KJSIT</p>
+                      <p className=" text-slate-900 underline">Bachelor of Technology, Computer Science</p>
+                    </div>
+                    <p className="w-content">2015 - 2021</p>
+                  </div>
+                  <p className="text-slate-700 pl-2" style={{ fontSize: '0.9rem' }}>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </section>
+                <section className="w-full flex-col justify-start items-start">
+                  <div className="flex justify-between items-start py-1">
+                    <div className="flex flex-col">
+                      <p className="font-semibold">KJSIT</p>
+                      <p className="text-sm text-slate-900 underline">Bachelor of Technology, Computer Science</p>
+                    </div>
+                    <p className="w-content">2015 - 2021</p>
+                  </div>
+                  <p className="text-slate-700 pl-2 text-sm" style={{ fontSize: '0.9rem' }}>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </section>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            </section>
+            <section className="w-full flex-col justify-start items-start">
+              <p className="font-bold text-lg uppercase">Skills</p>
+              <div className="w-12 h-1 bg-slate-900 rounded"></div>
+              <div className="flex flex-wrap gap-2 py-3 ">
+                <div className="border border-1 border-slate-300 px-3  rounded-lg w-fit"
+                  style={{ fontSize: '0.9rem' }}
+                >
+                  NextJS
+                </div>
+                <div className="border border-1 border-slate-300 px-3  rounded-lg w-fit"
+                  style={{ fontSize: '0.9rem' }}
+                >
+                  NextJS
+                </div>
+                <div className="border border-1 border-slate-300 px-3  rounded-lg w-fit"
+                  style={{ fontSize: '0.9rem' }}
+                >
+                  NextJS
+                </div>
+              </div>
+
+            </section>
+            <section className="w-full flex-col justify-start items-start">
+              <p className="font-bold text-lg uppercase">Language</p>
+              <div className="w-12 h-1 bg-slate-900 rounded"></div>
+              <ul className="flex flex-wrap gap-2 py-3 list-disc px-6 gap-6">
+                <li style={{ fontSize: '0.9rem' }}>
+                  Hello
+                </li>
+                <li style={{ fontSize: '0.9rem' }}>
+                  Hello
+                </li>
+                <li style={{ fontSize: '0.9rem' }}>
+                  Hello
+                </li>
+                <li style={{ fontSize: '0.9rem' }}>
+                  Hello
+                </li>
+                <li style={{ fontSize: '0.9rem' }}>
+                  Hello
+                </li>
+                <li style={{ fontSize: '0.9rem' }}>
+                  Hello
+                </li>
+              </ul>
+
+            </section>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="w-1/2 h-full bg-white rounded-md">
+
+      </div>
+
     </div>
   );
 }

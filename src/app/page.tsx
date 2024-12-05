415afda05fb6274db2faccfa8669ac04fef3dd17
Yash KC <yashchauhan.coder@gmail.com>
2024-12-05 15:37:03 +0530

    feat:alert_and_loaders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,8 @@ import { Checkbox } from "@/components/ui/checkbox"
 import ChooseTemplate from "@/components/ChooseTemplate";
 import Resume1 from "@/components/resume/Resume1";
 import axios from "axios"
+import { Toaster } from "@/components/ui/toaster"
+import { useToast } from "@/hooks/use-toast"
 
 
 
@@ -62,17 +64,37 @@ export default function Home() {
   const [sections, setSections] = useState(['Skill', 'Experience', 'Education', 'Certificate'])
   const [current, setCurrent] = useState<any>(['', {}, { isEdit: -1 }])
   const [edit, setEdit] = useState<any>(['', {}, 0])
+  const { toast } = useToast()
+  const [loading, setLoading] = useState(false);
 
 
   async function fetchResume() {
     const resume_id = localStorage.getItem('resume_id');
     if (resume_id) {
-      await axios.get(`/api/resume?id=${resume_id}`)
-        .then((res) => {
-          if (res.data.status == 200) {
-            setData(res.data.resume.data);
-          }
+      setLoading(true);
+      try {
+        await axios.get(`/api/resume?id=${resume_id}`)
+          .then((res) => {
+            if (res.data.status == 200) {
+              setData(res.data.resume.data);
+            }
+            else {
+              toast({
+                title: "Error",
+                description: "Error in fetching Resume. Please try again!",
+                variant: "destructive"
+              })
+            }
+          })
+      } catch (error) {
+        toast({
+          title: "Error",
+          description: "Error in fetching Resume. Please try again!",
+          variant: "destructive"
         })
+      } finally {
+        setLoading(false);
+      }
     }
   }
 
@@ -121,9 +143,6 @@ export default function Home() {
         dataClone[current[0]] = current[1];
         setData({ ...dataClone })
       }
-      // else {
-      //   setData({ ...data, [current[0]]: [...data[current[0]], current[1]] })
-      // }
     }
     setOpen(current[0]);
     setCurrent(['', {}, { isEdit: -1 }]);
@@ -132,6 +151,7 @@ export default function Home() {
 
   async function saveResumeToDB(dataClone: any) {
     try {
+      setLoading(true);
       var resumeId = localStorage.getItem("resume_id");
       if (resumeId) {
         await axios.put('/api/resume', {
@@ -140,10 +160,18 @@ export default function Home() {
         })
           .then((data) => {
             if (data.status == 200) {
-              console.log(data);
-              window.alert('success');
+              toast({
+                title: "Success",
+                description: "Data has been saved successfully!",
+              })
             }
-
+            else {
+              toast({
+                title: "Error",
+                description: "Error in saving data. Please try again!",
+                variant: "destructive"
+              })
+            }
           })
       }
       else {
@@ -153,16 +181,31 @@ export default function Home() {
         })
           .then((data) => {
             if (data.status == 201) {
-              console.log(data);
-              window.alert('success');
+              toast({
+                title: "Success",
+                description: "Data has been saved successfully!",
+              })
               localStorage.setItem("resume_id", data.data.id);
+            }
+            else {
+              toast({
+                title: "Error",
+                description: "Error in saving data. Please try again!",
+                variant: "destructive"
+              })
             }
           })
       }
 
     } catch (error) {
-      console.log('some error occurred');
-
+      toast({
+        title: "Error",
+        description: "Error in saving data. Please try again!",
+        variant: "destructive"
+      })
+    }
+    finally {
+      setLoading(false);
     }
   }
 
@@ -779,6 +822,22 @@ export default function Home() {
           :
           <></>
       }
+      <Toaster />
+      {loading ?
+        <div className="fixed w-full h-screen flex justify-center items-center"
+          style={{ backgroundColor: 'rgba(0,0,0,0.3)' }}
+        >
+          <div role="status">
+            <svg aria-hidden="true" className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+        : <></>
+      }
+
     </div >
   );
 }
--- src/components/resume/Resume1.tsx
+++ src/components/resume/Resume1.tsx
@@ -145,7 +145,7 @@ export default function Resume1({ dummy, data }: any) {
                                                         <p className="font-semibold ">{exp?.school}</p>
                                                         <p className=" text-slate-900 underline">{exp?.degree}</p>
                                                     </div>
-                                                    <p className="w-content">{formatDateToMonthYear(exp?.start) + " - " + exp?.present ? 'Present' : formatDateToMonthYear(exp?.end)}</p>
+                                                    <p className="w-content">{formatDateToMonthYear(exp?.start) + " - " + (exp?.present ? 'Present' : formatDateToMonthYear(exp?.end))}</p>
                                                 </div>
                                                 <p className="text-slate-700 pl-2" style={{ fontSize: '0.9rem' }} dangerouslySetInnerHTML={{ __html: exp?.description }}>
                                                     {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. */}
